da7ff6dfad7bd84286f8868eb9bf9b4bdb7d8225
felipemrvieira <felipemrvieira@gmail.com>
2022-02-21 16:32:00 -0300

    Adjust test

--- test-utils/server-handlers.ts
+++ test-utils/server-handlers.ts
@@ -2,7 +2,7 @@ import { rest } from "msw";
 
 import { mockedLaunchs } from "../__mocks__/mocks";
 
-const API_URL = "https://api.spacexdata.com/v3/launches/past/?limit=4&offset=0";
+const API_URL = "https://api.spacexdata.com/v3/launches";
 
 const handlers = [
   rest.get(API_URL, (_req, res, ctx) => {
